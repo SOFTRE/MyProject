4810c7e4f0a5b18cc25b11aa11f616b45419feba
SOFTRE <39897273+SOFTRE@users.noreply.github.com>
2020-10-27 22:23:49 +0800

    one commit
    one commit

---
+++ demo01/src/main/java/demo01.java
@@ -0,0 +1,12 @@
+/**
+ * @author ywliu
+ * @Title: demo01
+ * @Package
+ * @Description:
+ * @date 2020/10/2721:12
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println(1/3.0d);
+    }
+}
